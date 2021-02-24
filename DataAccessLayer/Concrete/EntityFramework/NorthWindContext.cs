3ffb6bd10583e8322bc775c30a660eb6e0a09d36
Orçun Karakaya <orcunkarakaya00@gmail.com>
2021-02-24 21:17:00 +0300

    project name changed

--- DataAccessLayer/Concrete/EntityFramework/NorthWindContext.cs
+++ DataAccessLayer/Concrete/EntityFramework/NorthWindContext.cs
@@ -6,9 +6,7 @@ namespace DataAccessLayer.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"\Server = 175.45.2.12");/* 1.30  */
-
-            //https://www.youtube.com/watch?v=ow-EHetuNAU&feature=emb_logo&ab_channel=EnginDemiro%C4%9F
+            optionsBuilder.UseSqlServer(@"\Server = 175.45.2.12");
         }
     }
 }
