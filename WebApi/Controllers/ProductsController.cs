6b971c458b504894e07242471acf40e321adc300
Orçun Karakaya <orcunkarakaya00@gmail.com>
2021-03-13 22:24:20 +0300

    added web api layer and configure service implementation

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
